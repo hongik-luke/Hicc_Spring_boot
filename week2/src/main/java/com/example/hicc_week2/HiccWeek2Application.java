c4a83ebdc657d5be40a8147d450ca979aab02cbe
Jeongyeop Lyou <amused170@gmail.com>
2026-03-31 17:12:51 +0900

    week2 folder add whit DB connection

---
+++ week2/src/main/java/com/example/hicc_week2/HiccWeek2Application.java
@@ -0,0 +1,13 @@
+package com.example.hicc_week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiccWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiccWeek2Application.class, args);
+    }
+
+}
---
+++ week2/src/test/java/com/example/hicc_week2/HiccWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hicc_week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiccWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
