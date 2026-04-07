7b036fcef15dcf95680597c9a20c4c80790831b8
김기현 <luke0985>
2026-04-07 12:22:26 +0900

    test: 엔티티 매핑 및 JPA 스캔 확인 테스트 추가

---
+++ Gihyeon/Week3/SpringStudy/src/test/java/com/luke/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.luke.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
