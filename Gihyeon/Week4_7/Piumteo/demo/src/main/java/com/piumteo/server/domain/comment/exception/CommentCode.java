641692327312aa76bce2df6353564f9b3e933269
김기현 <luke0985>
2026-05-19 17:50:05 +0900

    chore : Comment의 Code 추가

--- Gihyeon/Week4_7/Piumteo/demo/src/main/java/com/piumteo/server/domain/comment/exception/CommentCode.java
+++ Gihyeon/Week4_7/Piumteo/demo/src/main/java/com/piumteo/server/domain/comment/exception/CommentCode.java
@@ -11,14 +11,14 @@ public enum CommentCode implements ErrorCode {
 
     COMMENT_LIST_SEARCH_SUCCESS(
             HttpStatus.OK,
-            "COMMENT_200_001",
+            "COMMENT_200_000",
             "댓글 목록 조회에 성공했습니다."
     ),
 
-    COMMENT_CREATE_SUCCESS(
-            HttpStatus.CREATED,
-            "COMMENT_201_001",
-            "댓글 작성에 성공했습니다."
+    COMMENT_UPDATE_SUCCESS(
+            HttpStatus.OK,
+            "COMMENT_200_001",
+            "댓글 수정에 성공했습니다."
     ),
 
     COMMENT_DELETE_SUCCESS(
@@ -27,33 +27,39 @@ public enum CommentCode implements ErrorCode {
             "댓글 삭제에 성공했습니다."
     ),
 
+    COMMENT_CREATE_SUCCESS(
+            HttpStatus.CREATED,
+            "COMMENT_201_001",
+            "댓글 작성에 성공했습니다."
+    ),
+
     COMMENT_NOT_FOUND(
             HttpStatus.NOT_FOUND,
             "COMMENT_404_001",
-            "댓글을 찾을 수 없습니다."
+            "존재하지 않는 댓글입니다."
     ),
 
-    COMMENT_ALREADY_DELETED(
-            HttpStatus.CONFLICT,
-            "COMMENT_409_001",
-            "이미 삭제된 댓글입니다."
-    ),
-
-    INVALID_GUEST_PASSWORD(
-            HttpStatus.BAD_REQUEST,
-            "COMMENT_400_001",
-            "게스트 비밀번호가 일치하지 않습니다."
-    ),
-
-    UNAUTHORIZED_COMMENT_DELETE(
+    COMMENT_UPDATE_FORBIDDEN(
             HttpStatus.FORBIDDEN,
             "COMMENT_403_001",
-            "댓글을 삭제할 권한이 없습니다."
+            "해당 댓글을 수정할 권한이 없습니다."
+    ),
+
+    COMMENT_DELETE_FORBIDDEN(
+            HttpStatus.FORBIDDEN,
+            "COMMENT_403_002",
+            "해당 댓글을 삭제할 권한이 없습니다."
+    ),
+
+    COMMENT_PASSWORD_MISMATCH(
+            HttpStatus.FORBIDDEN,
+            "COMMENT_403_003",
+            "댓글 비밀번호가 일치하지 않습니다."
     ),
 
     INVALID_COMMENT_AUTHOR_TYPE(
             HttpStatus.BAD_REQUEST,
-            "COMMENT_400_002",
+            "COMMENT_400_001",
             "댓글 작성자 타입이 올바르지 않습니다."
     );
 
